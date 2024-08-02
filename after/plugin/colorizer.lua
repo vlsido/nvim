2f1ca0f5339ad11633c1a2561d6cb656b1a0a41c
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2024-08-03 02:11:44 +0300

    perf: format

--- after/plugin/colorizer.lua
+++ after/plugin/colorizer.lua
@@ -1,10 +1,9 @@
 function Colorize(color)
-	color = color or 'rose-pine'
+	color = color or "rose-pine"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none'})
-	vim.api.nvim_set_hl(0, 'NormalFloat', {})
-
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", {})
 end
 
-Colorize('rose-pine')
+Colorize("rose-pine")
