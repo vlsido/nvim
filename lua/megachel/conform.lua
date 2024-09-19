86e0aeefbac60cfb0350766e1d713ee42ef04269
parsivoe <parsivoe@parsivoe-iMac.local>
2024-09-19 22:47:57 +0300

    initialize git repo

---
+++ lua/megachel/conform.lua
@@ -0,0 +1,26 @@
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<C-f>",
+	":lua vim.lsp.buf.format({ async = true })<CR>",
+	{ noremap = true, silent = true }
+)
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
