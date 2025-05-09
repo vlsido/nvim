1e7aca5ddde0dd582d7aca3ca81074f05805ff61
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-05-09 23:20:24 +0300

    perf: open dap-ui on events

--- lua/megachel/dap.lua
+++ lua/megachel/dap.lua
@@ -1,5 +1,7 @@
 
-require("dap").adapters.codelldb = {
+local dap, dapui = require("dap"), require("dapui")
+
+dap.adapters.codelldb = {
  type = "server",
  port = "${port}",
  executable = {
@@ -7,6 +9,21 @@ require("dap").adapters.codelldb = {
   args = { "--port", "${port}" },
  },
 }
+
+dapui.setup()
+
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
 
 require("nvim-dap-virtual-text").setup {
     enabled = true,                        -- enable this plugin (the default)
