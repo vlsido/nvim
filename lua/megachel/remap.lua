7c12953a042a5ae520defdab9e4dc25e36749a0f
Vladislav Sidorenko <vladislavsidorenko2000@gmail.com>
2025-09-21 10:19:39 +0300

    feat: window remaps

--- lua/megachel/remap.lua
+++ lua/megachel/remap.lua
@@ -51,17 +51,37 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 -- /* Windows size */
 -- NOPs
 vim.keymap.set("n", "<C-v>", "<nop>")
-vim.keymap.set("n", "<C-.>", "<nop>")
+vim.keymap.set("n", "<C-v>", "<nop>")
+vim.keymap.set("n", "<", "<nop>")
+vim.keymap.set("n", ">", "<nop>")
 vim.keymap.set("n", ".", "<nop>")
 vim.keymap.set("n", ";", "<nop>")
 vim.keymap.set("n", "'", "<nop>")
+vim.keymap.set("n", "<leader><Left>", "<nop>")
+vim.keymap.set("n", "<leader><Right>", "<nop>")
+vim.keymap.set("n", "<leader><Up>", "<nop>")
+vim.keymap.set("n", "<leader><Down>", "<nop>")
 -- Mappings
-vim.keymap.set("n", "<C-,>", "<C-w>10<")
-vim.keymap.set("n", "<C-.>", "<C-w>10>")
-vim.keymap.set("n", "<C-;>", "<C-w>3+")
-vim.keymap.set("n", "<C-'>", "<C-w>3-")
-
+vim.keymap.set({ "n", "v" }, "<", "<C-w>10<")
+vim.keymap.set({ "n", "v" }, ">", "<C-w>10>")
+vim.keymap.set({ "n", "v" }, "}", "<C-w>3+")
+vim.keymap.set({ "n", "v" }, "{", "<C-w>3-")
+vim.keymap.set({ "n", "v" }, "{", "<C-w>3-")
+vim.keymap.set({ "n", "v" }, "{", "<C-w>3-")
+vim.keymap.set("n", "<leader><Left>", "<C-w>h")
+vim.keymap.set("n", "<leader><Right>", "<C-w>l")
+vim.keymap.set("n", "<leader><Up>", "<C-w>k")
+vim.keymap.set("n", "<leader><Down>", "<C-w>l")
 -- [END] Windows size
+
+vim.keymap.set({ "n", "v" }, "<C-,>", "<nop>")
+vim.keymap.set({ "n", "v" }, "<C-.>", "<nop>")
+
+vim.keymap.set({ "n", "v" }, "<C-,>", "^")
+vim.keymap.set({ "n", "v" }, "<C-.>", "$")
+
+vim.keymap.set("i", "<C-,>", "<C-c>^i")
+vim.keymap.set("i", "<C-.>", "<C-c>$i")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
--- lua/megachel/tree.lua
+++ lua/megachel/tree.lua
@@ -9,31 +9,39 @@ vim.api.nvim_set_keymap("n", "<C-h>", ":NvimTreeToggle<cr>", { silent = true, no
 vim.api.nvim_set_keymap("n", "<C-c>", ":NvimTreeFocus<cr>", { silent = true, noremap = true })
 
 local function attachThisLeaf(bufnr)
-  local api = require("nvim-tree.api")
+	local api = require("nvim-tree.api")
 
-  local function opts(desc)
-    return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
+	local function opts(desc)
+		return { desc = desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
 
-  -- default mappings
-  api.config.mappings.default_on_attach(bufnr)
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
 
-  -- my mappings
-  vim.keymap.set("n", "<C-]>", "gt", opts("Mock prev tab"))
-  vim.keymap.set("n", "<C-[>", "gT", opts("Mock next tab"))
+	-- my mappings
+	vim.keymap.set("n", "<C-]>", "gt", opts("Mock prev tab"))
+	vim.keymap.set("n", "<C-[>", "gT", opts("Mock next tab"))
+
+	vim.keymap.set({ "n", "v" }, "<", "<nop>", opts("Mock prev tab"))
+	vim.keymap.set({ "n", "v" }, ">", "<nop>", opts("Mock next tab"))
+
+	vim.keymap.set({ "n", "v" }, "<", "<C-w>10<")
+	vim.keymap.set({ "n", "v" }, ">", "<C-w>10>")
+	vim.keymap.set({ "n", "v" }, "}", "<C-w>3+")
+	vim.keymap.set({ "n", "v" }, "{", "<C-w>3-")
 end
 
 -- OR setup with some options
 require("nvim-tree").setup({
-  on_attach = attachThisLeaf,
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+	on_attach = attachThisLeaf,
+	sort_by = "case_sensitive",
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
 })
